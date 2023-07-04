1e204896330efd7f5679ea776ba821f86f57f9a4
luqmanAH <luqman.alhelmy@gmail.com>
2023-07-04 22:33:37 +0700

    On branch OOP_Num_Game Changes to be committed: 	modified:   Day1/obj/Debug/net7.0/Day1.assets.cache 	deleted:    Day2/bin/Debug/net7.0/Day2.dll 	deleted:    Day2/obj/Debug/net7.0/Day2.AssemblyInfoInputs.cache 	modified:   Day2/obj/Debug/net7.0/Day2.assets.cache 	deleted:    Day2/obj/Debug/net7.0/Day2.csproj.AssemblyReference.cache 	deleted:    Day2/obj/Debug/net7.0/Day2.csproj.CoreCompileInputs.cache 	modified:   Day2/obj/Debug/net7.0/Day2.csproj.FileListAbsolute.txt 	deleted:    Day2/obj/Debug/net7.0/Day2.dll 	deleted:    Day2/obj/Debug/net7.0/Day2.genruntimeconfig.cache 	deleted:    Day2/obj/Debug/net7.0/ref/Day2.dll 	deleted:    Day2/obj/Debug/net7.0/refint/Day2.dll 	new file:   OffClassExercises/OOP_Num_Game/GameData.cs 	new file:   OffClassExercises/OOP_Num_Game/GameLogic.cs 	new file:   OffClassExercises/OOP_Num_Game/OOP_Num_Game.csproj 	new file:   OffClassExercises/OOP_Num_Game/Program.cs 	new file:   OffClassExercises/OOP_Num_Game/UserCLI.cs 	renamed:    Day2/bin/Debug/net7.0/Day2.deps.json -> OffClassExercises/OOP_Num_Game/bin/Debug/net7.0/OOP_Num_Game.deps.json 	new file:   OffClassExercises/OOP_Num_Game/bin/Debug/net7.0/OOP_Num_Game.dll 	renamed:    Day2/bin/Debug/net7.0/Day2.exe -> OffClassExercises/OOP_Num_Game/bin/Debug/net7.0/OOP_Num_Game.exe 	renamed:    Day2/obj/Debug/net7.0/Day2.pdb -> OffClassExercises/OOP_Num_Game/bin/Debug/net7.0/OOP_Num_Game.pdb 	renamed:    Day2/bin/Debug/net7.0/Day2.runtimeconfig.json -> OffClassExercises/OOP_Num_Game/bin/Debug/net7.0/OOP_Num_Game.runtimeconfig.json 	new file:   OffClassExercises/OOP_Num_Game/obj/Debug/net7.0/.NETCoreApp,Version=v7.0.AssemblyAttributes.cs 	renamed:    Day2/obj/Debug/net7.0/Day2.AssemblyInfo.cs -> OffClassExercises/OOP_Num_Game/obj/Debug/net7.0/OOP_Num_Game.AssemblyInfo.cs 	new file:   OffClassExercises/OOP_Num_Game/obj/Debug/net7.0/OOP_Num_Game.AssemblyInfoInputs.cache 	renamed:    Day2/obj/Debug/net7.0/Day2.GeneratedMSBuildEditorConfig.editorconfig -> OffClassExercises/OOP_Num_Game/obj/Debug/net7.0/OOP_Num_Game.GeneratedMSBuildEditorConfig.editorconfig 	new file:   OffClassExercises/OOP_Num_Game/obj/Debug/net7.0/OOP_Num_Game.GlobalUsings.g.cs 	new file:   OffClassExercises/OOP_Num_Game/obj/Debug/net7.0/OOP_Num_Game.assets.cache 	new file:   OffClassExercises/OOP_Num_Game/obj/Debug/net7.0/OOP_Num_Game.csproj.AssemblyReference.cache 	new file:   OffClassExercises/OOP_Num_Game/obj/Debug/net7.0/OOP_Num_Game.csproj.CoreCompileInputs.cache 	new file:   OffClassExercises/OOP_Num_Game/obj/Debug/net7.0/OOP_Num_Game.csproj.FileListAbsolute.txt 	new file:   OffClassExercises/OOP_Num_Game/obj/Debug/net7.0/OOP_Num_Game.dll 	new file:   OffClassExercises/OOP_Num_Game/obj/Debug/net7.0/OOP_Num_Game.genruntimeconfig.cache 	renamed:    Day2/bin/Debug/net7.0/Day2.pdb -> OffClassExercises/OOP_Num_Game/obj/Debug/net7.0/OOP_Num_Game.pdb 	new file:   OffClassExercises/OOP_Num_Game/obj/Debug/net7.0/apphost.exe 	new file:   OffClassExercises/OOP_Num_Game/obj/Debug/net7.0/ref/OOP_Num_Game.dll 	new file:   OffClassExercises/OOP_Num_Game/obj/Debug/net7.0/refint/OOP_Num_Game.dll 	new file:   OffClassExercises/OOP_Num_Game/obj/OOP_Num_Game.csproj.nuget.dgspec.json 	new file:   OffClassExercises/OOP_Num_Game/obj/OOP_Num_Game.csproj.nuget.g.props 	new file:   OffClassExercises/OOP_Num_Game/obj/OOP_Num_Game.csproj.nuget.g.targets 	new file:   OffClassExercises/OOP_Num_Game/obj/project.assets.json 	new file:   OffClassExercises/OOP_Num_Game/obj/project.nuget.cache 	modified:   Solution Folder.sln

---
+++ OffClassExercises/OOP_Num_Game/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography.X509Certificates;
+using GameDataLibrary;
+using GameLogicLibrary;
+using UserInterfaceLibrary;
+public class Program
+{
+    public static void Main()
+    {
+        GameLogic gameLogic = new();
+        UserInterface userInterface = new();
+        GameData gameData = new(1, 100);
+        bool restart = true;
+
+        while(restart)
+        {
+            int guess = gameData.guess;
+            int guessCount = gameData.guessCount;
+            int randomNumber = gameLogic.RandomNumberGenerator(gameData.MinimumNumber, gameData.MaximumNumber);
+
+            while (!gameLogic.IsGuessCorrect(guess))
+            {
+                userInterface.DisplayName(gameData.MinimumNumber, gameData.MaximumNumber);
+                guess = userInterface.GetGuess();
+                Console.WriteLine("Guess: " + guess);
+
+                if (guess > randomNumber)
+                {
+                    Console.WriteLine($"{guess} is to high from the actual number!");
+                }
+                else if (guess < randomNumber)
+                {
+                    Console.WriteLine($"{guess} is to low from the actual number!");
+                }
+                guessCount ++;
+            }
+            userInterface.DisplayFinish(randomNumber,guessCount);
+            string? response = userInterface.ExitPrompt();
+
+            if (response == "Y")
+            {
+                restart = true;
+            }
+            else
+            {
+                restart = false;
+            }
+        }
+    }
+}
