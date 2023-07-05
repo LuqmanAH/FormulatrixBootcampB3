0b78cffc28ad7f8ac613b0b5fc7b5bbd9c3d4a4f
LuqmanAH <luqman.alhelmy@gmail.com>
2023-07-05 11:48:12 +0700

    Changes to be committed: 	modified:   Day1/obj/Debug/net7.0/Day1.GeneratedMSBuildEditorConfig.editorconfig 	modified:   Day1/obj/Debug/net7.0/Day1.assets.cache 	new file:   Day2/obj/Debug/net7.0/Day2.AssemblyInfo.cs 	new file:   Day2/obj/Debug/net7.0/Day2.AssemblyInfoInputs.cache 	new file:   Day2/obj/Debug/net7.0/Day2.GeneratedMSBuildEditorConfig.editorconfig 	modified:   Day2/obj/Debug/net7.0/Day2.assets.cache 	new file:   Day2/obj/Debug/net7.0/Day2.csproj.AssemblyReference.cache 	new file:   Day3/Day3.csproj 	new file:   Day3/Program.cs 	new file:   Day3/bin/Debug/net7.0/Day3.deps.json 	new file:   Day3/bin/Debug/net7.0/Day3.dll 	new file:   Day3/bin/Debug/net7.0/Day3.exe 	new file:   Day3/bin/Debug/net7.0/Day3.pdb 	new file:   Day3/bin/Debug/net7.0/Day3.runtimeconfig.json 	new file:   Day3/obj/Day3.csproj.nuget.dgspec.json 	new file:   Day3/obj/Day3.csproj.nuget.g.props 	new file:   Day3/obj/Day3.csproj.nuget.g.targets 	new file:   Day3/obj/Debug/net7.0/.NETCoreApp,Version=v7.0.AssemblyAttributes.cs 	new file:   Day3/obj/Debug/net7.0/Day3.AssemblyInfo.cs 	new file:   Day3/obj/Debug/net7.0/Day3.AssemblyInfoInputs.cache 	new file:   Day3/obj/Debug/net7.0/Day3.GeneratedMSBuildEditorConfig.editorconfig 	new file:   Day3/obj/Debug/net7.0/Day3.GlobalUsings.g.cs 	new file:   Day3/obj/Debug/net7.0/Day3.assets.cache 	new file:   Day3/obj/Debug/net7.0/Day3.csproj.AssemblyReference.cache 	new file:   Day3/obj/Debug/net7.0/Day3.csproj.CoreCompileInputs.cache 	new file:   Day3/obj/Debug/net7.0/Day3.csproj.FileListAbsolute.txt 	new file:   Day3/obj/Debug/net7.0/Day3.dll 	new file:   Day3/obj/Debug/net7.0/Day3.genruntimeconfig.cache 	new file:   Day3/obj/Debug/net7.0/Day3.pdb 	new file:   Day3/obj/Debug/net7.0/apphost.exe 	new file:   Day3/obj/Debug/net7.0/ref/Day3.dll 	new file:   Day3/obj/Debug/net7.0/refint/Day3.dll 	new file:   Day3/obj/project.assets.json 	new file:   Day3/obj/project.nuget.cache 	modified:   OffClassExercises/Number_Game/obj/Debug/net7.0/Number_Game.GeneratedMSBuildEditorConfig.editorconfig 	modified:   OffClassExercises/Number_Game/obj/Debug/net7.0/Number_Game.assets.cache 	modified:   OffClassExercises/Number_Game/obj/Debug/net7.0/Number_Game.csproj.AssemblyReference.cache 	modified:   OffClassExercises/OOP_Num_Game/OOP_Num_Game.csproj 	modified:   OffClassExercises/OOP_Num_Game/obj/Debug/net7.0/OOP_Num_Game.GeneratedMSBuildEditorConfig.editorconfig 	modified:   OffClassExercises/OOP_Num_Game/obj/Debug/net7.0/OOP_Num_Game.assets.cache 	modified:   OffClassExercises/OOP_Num_Game/obj/Debug/net7.0/OOP_Num_Game.csproj.AssemblyReference.cache 	modified:   OffClassExercises/hello/obj/Debug/net7.0/hello.GeneratedMSBuildEditorConfig.editorconfig 	modified:   OffClassExercises/hello/obj/Debug/net7.0/hello.assets.cache 	modified:   OffClassExercises/hello/obj/Debug/net7.0/hello.csproj.AssemblyReference.cache 	modified:   Solution Folder.sln

---
+++ Day3/Program.cs
@@ -0,0 +1,115 @@
+﻿namespace BankWakanda;
+
+public interface ICloseAccount
+{
+    void CloseAccount();
+}
+public abstract class BankAccount
+{
+    public string AccountName {get; set;}
+    public int AccountNumber {get; set;}
+    public double AccountBalance {get; set;}
+    public BankAccount(string accountName, double accountBalance)
+    {
+        this.AccountName = accountName;
+        this.AccountBalance = accountBalance;
+        this.AccountNumber = 12345678;
+    }
+    public BankAccount()
+    {
+        this.AccountName = "FulanAlFulani";
+        this.AccountBalance = 100.0;
+    }
+    public BankAccount(string accountName, double accountBalance, int accountNumber)
+    {
+        this.AccountName = accountName;
+        this.AccountBalance = accountBalance;
+        this.AccountNumber = accountNumber;
+    }
+    public abstract double Deposit(double amount);
+    public abstract double Withdraw(double amount);
+}
+
+public class CheckingAccount : BankAccount, ICloseAccount
+{
+    public int overdraftLimit;
+    
+    public CheckingAccount(string accountName, int accountNumber, double accountBalance, int overdraftLimit): base(accountName, accountBalance, accountNumber)
+    {
+        this.overdraftLimit = overdraftLimit;
+    }
+
+    public override double Deposit(double amount)
+    {
+        this.AccountBalance += amount;
+        return AccountBalance;
+    }
+    public override double Withdraw(double amount)
+    {
+        if (this.AccountBalance - amount < 0 && this.overdraftLimit <= 0){
+            Console.WriteLine("Insufficient Funds!");
+            return AccountBalance;
+        }
+        this.AccountBalance -= amount;
+        return AccountBalance;
+    }
+    public void CloseAccount()
+    {
+        Console.WriteLine($"{this.AccountName} Account has successfully closed and your entire balance is now ours");
+    }
+}
+
+public class SavingsAccount : BankAccount
+{
+    public double interestRate;
+    public int savingDuration;
+    public double balanceAfterInterest = 0.0;
+
+    public SavingsAccount(string accountName, int accountNumber, double accountBalance, int savingDuration, double interestRate) : base(accountName, accountBalance, accountNumber)
+    {
+        this.savingDuration = savingDuration;
+        this.interestRate = interestRate;
+    }
+    public override double Deposit(double amount)
+    {
+        this.AccountBalance += amount;
+        return AccountBalance;
+    }
+    public override double Withdraw(double amount)
+    {
+        if (this.AccountBalance - amount < 0)
+        {
+            Console.WriteLine("Insufficient Funds!");
+            return AccountBalance;
+        }
+        this.AccountBalance -= amount;
+        return AccountBalance;
+    }
+    public double CalculateInterest(){
+        this.balanceAfterInterest = AccountBalance * savingDuration * interestRate;
+        return balanceAfterInterest;
+    }
+}
+
+public class program
+{
+    public static void Main()
+    {
+        // SavingAccount Instance
+        SavingsAccount savingsAccount1 = new SavingsAccount("Hendrik Sudrajat", 56478832, 45690.40, 5, 0.35);
+        double withdrawMoney = 100.0;
+        double depositMoney = 500.0;
+        double currBalance = savingsAccount1.Withdraw(withdrawMoney);
+        Console.WriteLine("Interest Earned: {0}", savingsAccount1.CalculateInterest());
+        Console.WriteLine(savingsAccount1.AccountBalance + "\n");
+        Console.WriteLine(
+            $"Successfully withdrawn {withdrawMoney} to {savingsAccount1.AccountName}. Balance is now {currBalance}");
+        double depoBalance = savingsAccount1.Deposit(depositMoney);
+        Console.WriteLine(
+            $"Successfully deposited {depositMoney} to {savingsAccount1.AccountName}. Balance is now {depoBalance}\n");
+
+        // CheckingAccount Instance
+        CheckingAccount checkAccount1 = new CheckingAccount("Slamet Riyadi", 678832, 145690.40, -345);
+        checkAccount1.CloseAccount();
+    }
+}
