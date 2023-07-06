e3b4b07302af4635f7de9bab5d196e94308da4b9
luqmanAH <luqman.alhelmy@gmail.com>
2023-07-07 06:41:58 +0700

    On branch Day4Revise some new comments 1 bug detected

--- Day4New/Program.cs
+++ Day4New/Program.cs
@@ -12,10 +12,16 @@ public class MainProgram
         bookShelf1.AddItem(new NonFictionBook( 54, "2020", "Albert Camus", "The absurd life"));
         bookShelf1.AddItem(new ElectronicThesis(180,"2018", "Muhammad Sulaiman Rosyid", "Aplikasi Fast Fourier Transform pada Analisis Spektrum Sinyal Cinta", ".pdf"));
 
+        //! List items refer to parent interface rather than its child, this makes methods derived from child interface unaccessible
+        // TODO: resolve error by altering program.cs or the interface source code
+        
         List<ILibraryItem> items = bookShelf1.GetItems();
 
         Console.WriteLine(items.Count);
         Console.WriteLine(items[0].GetTitle());
+        //* Line below will result in build failure when uncommented
+        // Console.WriteLine(items[0].CoverType());
+
         Console.WriteLine(items[1].GetTitle());
     }
 }
