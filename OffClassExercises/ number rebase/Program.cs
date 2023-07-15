6caa19aa190c5eac01c96a59fe498a42f81f9c24
luqmanAH <luqman.alhelmy@gmail.com>
2023-07-15 21:48:18 +0700

    initiated rebase project, completed any to base 10 funct

---
+++ OffClassExercises/ number rebase/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace NumberRebase;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        NumberRebase rebaser = new();
+        double result = rebaser.AnyToBase10(5, 10);
+        Console.WriteLine(result);
+    }
+}
+
+class NumberRebase
+{
+    
+    public double AnyToBase10(double initbase, int inputNum)
+    {
+        double y = 0d;
+		List<int> digits = new List<int>();
+		List<double> digitsReady = new List<double>();
+		
+		do
+		{
+			int digit = inputNum % 10;
+			digits.Add(digit);
+            inputNum /= 10;
+
+		}while(inputNum > 0);
+
+		foreach (int digit in digits)
+		{
+			int index = digits.IndexOf(digit);
+			double newDigit = digit * Math.Pow(initbase, index);
+			y += newDigit;
+			digitsReady.Add(newDigit);
+		}
+        return y;
+    }
+}
