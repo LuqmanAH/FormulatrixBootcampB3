dc235b20f284bfe3abbff21be411aea893ac41cd
luqmanAH <luqman.alhelmy@gmail.com>
2023-07-15 22:31:01 +0700

    Main logic complete, but still trash

--- OffClassExercises/ number rebase/Program.cs
+++ OffClassExercises/ number rebase/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Xml.XPath;
 
 namespace NumberRebase;
 
@@ -7,15 +8,24 @@ class Program
     static void Main(string[] args)
     {
         NumberRebase rebaser = new();
-        double result = rebaser.AnyToBase10(5, 10);
-        Console.WriteLine(result);
+        // double result = rebaser.AnyToBase10(3, 12);
+        // Console.WriteLine(result);
+		// double backAgain = rebaser.DecToAny(5, result);
+		// Console.WriteLine(backAgain);
+		double daBoi = rebaser.Rebase(3, 5, 12);
+		Console.WriteLine(daBoi);
     }
 }
 
 class NumberRebase
 {
-    
-    public double AnyToBase10(double initbase, int inputNum)
+    public double Rebase(double initBase, double toBase, int inputNum)
+	{
+		double decEquivalent = AnyToBase10(initBase, inputNum);
+		double result = DecToAny(toBase, decEquivalent);
+		return result;
+	}
+    private static double AnyToBase10(double initBase, int inputNum)
     {
         double y = 0d;
 		List<int> digits = new List<int>();
@@ -32,10 +42,27 @@ class NumberRebase
 		foreach (int digit in digits)
 		{
 			int index = digits.IndexOf(digit);
-			double newDigit = digit * Math.Pow(initbase, index);
+			double newDigit = digit * Math.Pow(initBase, index);
 			y += newDigit;
 			digitsReady.Add(newDigit);
 		}
         return y;
     }
+
+	private static double DecToAny(double toBase, double inputNum)
+	{
+		List<double> anyDigits = new List<double>();
+		if (inputNum == 0)
+		{
+			return 0;
+		}
+		while (inputNum > 0)
+		{
+			anyDigits.Add(inputNum % toBase);
+			inputNum = Math.Floor(inputNum / toBase);
+		}
+		anyDigits.Reverse();
+		string temp = string.Join("", anyDigits);
+		return double.Parse(temp);
+	}
 }
