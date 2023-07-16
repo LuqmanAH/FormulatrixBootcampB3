263e1754b594d762d6cc18e3ee2c5d9d0bdc2603
luqmanAH <luqman.alhelmy@gmail.com>
2023-07-16 16:41:46 +0700

    new exercise: differenceOfSquares from exercism

---
+++ OffClassExercises/DiffOfSquareSum/Program.cs
@@ -0,0 +1,37 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        DifferenceOfSquares differ = new();
+        Console.WriteLine(differ.CalculateSquareOfSum(3));
+        Console.WriteLine(differ.CalculateSumOfSquares(3));
+        Console.WriteLine(differ.CalculateDiffOfSquare(3));
+    }
+}
+
+public class DifferenceOfSquares
+{
+    public int CalculateSquareOfSum(int max)
+    {
+        int temp = 0;
+        for (int i = 1; i <= max; i++)
+        {
+            temp += i;
+        }
+        return temp * temp;
+    }
+    public int CalculateSumOfSquares(int max)
+    {
+        int tempSquared = 0;
+        for (int i = 1; i <= max; i++)
+        {
+            tempSquared += i * i;
+        }
+        return tempSquared;
+    }
+    public int CalculateDiffOfSquare(int max)
+    {
+        int diff = CalculateSquareOfSum(max) - CalculateSumOfSquares(max);
+        return diff;
+    }
+}
