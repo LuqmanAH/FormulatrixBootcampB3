33cd97a69128ce7f94769515b1a21825d9c670a9
LuqmanAH <luqman.alhelmy@gmail.com>
2023-07-18 11:41:39 +0700

    Input Day15 Projects

---
+++ Week3/Day15/TryQueueSortedListHashTable/Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+
+namespace TryCollectionsPt2;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //! no typesafety
+        Hashtable hashtable = new Hashtable
+        {
+            { 5, "item1" },
+            { 5f, "item2" },
+            { 5d, "item3" },
+            { 5m, "item4" },
+            { 5L, "item5" },
+            { "5", "item6" },
+            { true, "item7" }
+        };
+
+
+        //! print elements with no clear sorting
+        foreach (var element in hashtable)
+        {
+            Console.WriteLine(element);
+            // Console.WriteLine(element.Key); Error as the type of 'element' is object
+            // Console.WriteLine(element.Value);
+        }
+
+        Console.WriteLine();
+        SortedList sortedList = new()
+        {
+            { 5, "item1" },
+            { 6, "item2" },
+            { 7, "item3" }
+        };
+
+        foreach (DictionaryEntry element in sortedList)
+        {
+            Console.WriteLine(element.Key);
+        }
+
+        //! Compare data structure of Q and Stack
+        //* Q's First in is 'indexed' as the first item and stack's First in is stacked by other entry until it becomes the last 'indexed' item
+        Console.WriteLine();
+        Queue queue= new Queue();
+        for (int i = 1; i <= 10; i++)
+        {
+            queue.Enqueue(i);
+        }
+        foreach (var item in queue)
+        {
+            Console.WriteLine(item);
+        }
+        Console.WriteLine();
+
+        //* De-Q returns the value
+        var removeItem = queue.Dequeue();
+        Console.WriteLine(removeItem);
+
+        Console.WriteLine();
+        Stack stack = new Stack();
+        for (int i = 1; i <= 10; i++)
+        {
+            stack.Push(i);
+        }
+        foreach (var item in stack)
+        {
+            Console.WriteLine(item);
+        }
+        Console.WriteLine();
+
+        //* Pop returns the value
+        var removeItem2 = stack.Pop();
+        Console.WriteLine(removeItem2);
+
+    }
+}
---
+++ Week3/Day15/Valid Parentheses/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace VPSolution;
+
+/*
+    * Implement The solution for Valid Parentheses Problem in LeetCode
+    * Assumption: string s only consist of combination of parentheses, this makes string that contains other than parentheses invalid
+    *
+*/
+
+public class Program 
+{
+    public static void Main()
+    {
+        bool test1 = IsValid("()");
+        bool test2 = IsValid("()[]{}");
+        bool test3 = IsValid("(]");
+        bool test4 = IsValid("(");
+        bool test5 = IsValid("ad({})"); // invalid by introducing characters other than parentheses
+
+        Console.WriteLine(test1);
+        Console.WriteLine(test2);
+        Console.WriteLine(test3);
+        Console.WriteLine(test4);
+        Console.WriteLine(test5);
+    }
+    public static bool IsValid(string s)
+    {
+        Dictionary<char, char> Map = new()
+        {
+            [')'] = '(',
+            [']'] = '[',
+            ['}'] = '{'
+        };
+
+        Stack<char> stack = new();
+        foreach (var c in s)
+        {
+            if (!Map.ContainsKey(c))
+            {
+                stack.Push(c);
+                continue;
+            }
+            else if (stack.Count == 0 || stack.Pop() != Map[c])
+            {
+                return false;
+            }
+        }
+        return stack.Count == 0;
+    }
+}
