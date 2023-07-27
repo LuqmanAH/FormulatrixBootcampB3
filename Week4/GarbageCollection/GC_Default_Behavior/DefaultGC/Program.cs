fed096c6bb7e4d05e44821c1127d8fa2fcdebcac
LuqmanAH <luqman.alhelmy@gmail.com>
2023-07-27 12:09:08 +0700

    Initial Commit on Week4\GC

---
+++ Week4/GarbageCollection/GC_Default_Behavior/DefaultGC/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+
+public class TreasureBox
+{
+    private static int _InitialID = 0;
+    private int _TreasureBoxID;
+    private int _money;
+    private int _jewelery;
+
+    public TreasureBox(int _money, int _jewelery)
+    {
+        _InitialID ++;
+        this. _money = _money;
+        this._jewelery = _jewelery;
+        _TreasureBoxID = _InitialID;
+
+        Console.WriteLine($"Treasure created worth of {_money + _jewelery}");
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Stopwatch stopwatch= Stopwatch.StartNew();
+        GenerateTreasureBoxes();// * Keluar dari line ini, instances yang di define sudah tidak relevan
+                                // * Automatic Garbage disposal by GC, no explicit declaration required
+                                // ! Auto disposal: Wait gen 0 heap full
+        Console.WriteLine($"yaaa kira-kira: {stopwatch.Elapsed}");
+        Console.Read();
+    }
+
+    static void GenerateTreasureBoxes()
+    {
+        TreasureBox myTreasureBox1 = new TreasureBox(69,100);
+        TreasureBox myTreasureBox2 = new TreasureBox(69,100);
+        TreasureBox myTreasureBox3 = new TreasureBox(69,100);
+        TreasureBox myTreasureBox4 = new TreasureBox(69,100);
+        TreasureBox myTreasureBox5 = new TreasureBox(69,100);
+        TreasureBox myTreasureBox6 = new TreasureBox(69,100);
+        TreasureBox myTreasureBox7 = new TreasureBox(69,100);
+        TreasureBox myTreasureBox8 = new TreasureBox(69,100);
+        TreasureBox myTreasureBox9 = new TreasureBox(69,100);
+        TreasureBox myTreasureBox = new TreasureBox(69,100);
+    }
+
+}
---
+++ Week4/GarbageCollection/GC_IDisposables/GC_IDisposables/Program.cs
@@ -0,0 +1,86 @@
+﻿using System.Diagnostics;
+
+public class TreasureBox : IDisposable
+{
+    private static int _InitialID = 0;
+    private int _TreasureBoxID;
+    private int _money;
+    private int _jewelery;
+    private bool _disposedValue;
+
+    public TreasureBox(int _money, int _jewelery)
+    {
+        _InitialID ++;
+        this. _money = _money;
+        this._jewelery = _jewelery;
+        _TreasureBoxID = _InitialID;
+
+        Console.WriteLine($"Treasure created worth of {_money + _jewelery}");
+    }
+
+
+    protected virtual void Dispose(bool disposing)
+    {
+        //! todos written below used to prevent other from accessing destructed instance fields
+
+        if (!_disposedValue)
+        {
+            if (disposing)
+            {
+                // TODO: dispose managed state (managed objects)
+            }
+
+            // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+            // TODO: set large fields to null
+            Console.WriteLine($"Treasure {_TreasureBoxID} has been looted by the Disposer");
+            _disposedValue = true;
+        }
+    }
+
+    ~TreasureBox()
+    {
+        Dispose(false); // * Supaya tidak bisa dispose
+        Console.WriteLine($"Treasure {_TreasureBoxID} has been looted by the finalizer");
+    }
+    // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+    // ~TreasureBox()
+    // {
+    //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+    //     Dispose(disposing: false);
+    // }
+
+    public void Dispose()
+    {
+        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this); // * Supaya Tidak Invoke Finalizer
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        // Stopwatch stopwatch= Stopwatch.StartNew();
+        GenerateTreasureBoxes(); 
+         // * GC dengan Finalizer (Tidak safe)
+    
+        TreasureBox programTreasure1 = new TreasureBox(78, 96);
+        programTreasure1.Dispose(); //* GC dengan Dispose pattern
+
+        // stopwatch.Stop();
+        // Console.WriteLine($"yaaa kira-kira: {stopwatch.Elapsed}");
+        
+        Console.Read();
+    }
+
+    static void GenerateTreasureBoxes()
+    {
+        TreasureBox myTreasureBox1 = new TreasureBox(69,100);
+        TreasureBox myTreasureBox2 = new TreasureBox(69,100);
+        TreasureBox myTreasureBox3 = new TreasureBox(69,100);
+        TreasureBox myTreasureBox4 = new TreasureBox(69,100);
+        
+    }
+
+}
