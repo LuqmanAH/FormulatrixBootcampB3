0bbb459ca678dbf1d65fa4eee533d1053308ec93
LuqmanAH <luqman.alhelmy@gmail.com>
2023-08-16 12:31:21 +0700

    Design pattern material schema

---
+++ Week5/Composite Design Pattern/CompositeDesignPattern/ConventionalPiece.cs
@@ -0,0 +1,16 @@
+﻿namespace NoPattern;
+public class Piece
+{
+    public string? PieceName {get; private set;}
+
+    public Piece (string pieceName = "pion")
+    {
+        PieceName = pieceName;
+    }
+
+    public bool SetPieceName(string pieceName)
+    {
+        PieceName = pieceName;
+        return true;
+    }
+}
---
+++ Week5/Composite Design Pattern/CompositeDesignPattern/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CompositeDesignPattern;
+
+partial class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+
+    static void Display<T>(T value)
+    {
+        ConsoleColor prevColor = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(value);
+    }
+
+}
