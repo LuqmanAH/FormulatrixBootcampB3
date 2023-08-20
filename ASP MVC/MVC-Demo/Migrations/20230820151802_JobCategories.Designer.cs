1a41ef1a14160758b8dcb3cb28d6652b4005c80e
luqmanAH <luqman.alhelmy@gmail.com>
2023-08-20 22:48:19 +0700

    new migration: categories model ready, to implement in viewer and controller

--- ASP MVC/MVC-Demo/Migrations/20230820151802_JobCategories.Designer.cs
+++ ASP MVC/MVC-Demo/Migrations/20230820151802_JobCategories.Designer.cs
@@ -2,6 +2,7 @@
 using MVC_Demo.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MVC_Demo.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230820151802_JobCategories")]
+    partial class JobCategories
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.10");
---
+++ ASP MVC/MVC-Demo/Migrations/20230820151802_JobCategories.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVC_Demo.Migrations
+{
+    /// <inheritdoc />
+    public partial class JobCategories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "Jobs",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "jobCategories",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CategoryName = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_jobCategories", x => x.CategoryId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Jobs_CategoryId",
+                table: "Jobs",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Jobs_jobCategories_CategoryId",
+                table: "Jobs",
+                column: "CategoryId",
+                principalTable: "jobCategories",
+                principalColumn: "CategoryId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Jobs_jobCategories_CategoryId",
+                table: "Jobs");
+
+            migrationBuilder.DropTable(
+                name: "jobCategories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Jobs_CategoryId",
+                table: "Jobs");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Jobs");
+        }
+    }
+}
